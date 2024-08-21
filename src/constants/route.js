c4bb52e3800c8999932c97a00c95ba4d24554175
Onat Ribar <onatribar@proton.me>
2024-08-21 15:48:02 +0300

    router logic

---
+++ src/constants/route.js
@@ -0,0 +1,10 @@
+// import MovieList from '../components/MovieList.js';
+import MovieBox from '../components/MovieBox.js';
+// import SearchForm from '../components/SearchForm.js';
+
+export const ROUTES = [
+    {
+        path:"../pages/MovieDetail.js",
+        element:<MovieBox/>
+    }
+];
