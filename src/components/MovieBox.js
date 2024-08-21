019aff33f33eb3f3aa33cb8f28c78eb4bd624214
Onat Ribar <onatribar@proton.me>
2024-08-21 14:15:56 +0300

    integrate imdb link

--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -1,18 +1,24 @@
+// src/components/MovieBox.js
+import React from 'react';
 import './MovieBox.css';
 
 const MovieBox = ({ movie }) => {
-    const { Title, Year, Poster, Actors, Director } = movie;
-  
-    return (
-      <div className="movie-box">
-        <img src={Poster !== 'N/A' ? Poster : 'https://via.placeholder.com/150'} alt={`${Title} Poster`} />
-        <div className="movie-details">
-          <h3>{Title} ({Year})</h3>
-          <p><strong>Director:</strong> {Director}</p>
-          <p><strong>Actors:</strong> {Actors}</p>
-        </div>
-      </div>
-    );
+  const { Title, Year, Poster, Actors, Director, imdbID } = movie;
+
+  const handleClick = () => {
+    window.open(`https://www.imdb.com/title/${imdbID}`, '_blank', 'noopener,noreferrer');
   };
-  
-  export default MovieBox;
+
+  return (
+    <div className="movie-box" onClick={handleClick}>
+      <img src={Poster !== 'N/A' ? Poster : 'https://via.placeholder.com/150'} alt={`${Title} Poster`} />
+      <div className="movie-details">
+        <h3>{Title} ({Year})</h3>
+        <p><strong>Actors:</strong> {Actors}</p>
+        <p><strong>Director:</strong> {Director}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieBox;
